d32407d1942d4eae882bce007ca0dac1009a9880
Anup Khadka <akhadka.dev@gmail.com>
2019-02-24 02:49:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ function addNewLiOnClick() {
 function clearEmployeeListOnLinkClick() {
   let clearButton = document.querySelector('main a');
   clearButton.addEventListener('click', function(event) {
-    let ul = document.querySelector('ul.employee-list');
+    let lis = document.querySelectorAll('ul.employee-list li');
     for (let i = 0; i < ul.length; i++) {
       ul.removeChild(children[i]);
     }
