aa3788a92eff037f5e3a3cf80c6d7cfa79d3c876
Anup Khadka <akhadka.dev@gmail.com>
2019-02-24 04:12:15 +0000

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,36 @@ function preventRefreshOnSubmit(){
 }
 
 preventRefreshOnSubmit()
+
+function retrieveEmployeeInformation() {
+  return document.querySelector('input').value;
+}
+
+function addNewElementAsLi() {
+  let li = document.createElement('li');
+  li.innerHTML = retrieveEmployeeInformation();
+  let ul = document.querySelector('ul.employee-list');
+  ul.appendChild(li);
+}
+
+function addNewLiOnClick() {
+  let addButton = document.querySelector('main form input[type="submit"]');
+  addButton.addEventListener('click', function(event) {
+    console.log('calling addNewElementAsLi()');
+    addNewElementAsLi();
+    //document.querySelector('input').value = "";
+  });
+}
+
+
+function clearEmployeeListOnLinkClick() {
+  let clearButton = document.querySelector('main a');
+  clearButton.addEventListener('click', function(event) {
+    let ul = document.querySelector('ul.employee-list');
+    let lis = ul.querySelectorAll('li')
+    for (let i = 0; i < lis.length; i++) {
+      //ul.removeChild(ul.querySelector(`li:nth-child(${i})`));
+      lis[i].remove();
+    }
+  });
+}
