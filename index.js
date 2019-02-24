127ae3934766769e7851420c1882c3ddf7ba9b7e
Anup Khadka <akhadka.dev@gmail.com>
2019-02-24 03:32:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,8 +22,7 @@ function addNewLiOnClick() {
   addButton.addEventListener('click', function(event) {
     console.log('calling addNewElementAsLi()');
     addNewElementAsLi();
-    resetInput();
-    event.stopPropagation();
+    document.querySelector('input').value = "";
   });
 }
 
